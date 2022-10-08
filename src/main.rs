8ff0434c81a0f135a0964dfea213df48158a977d
--replace-all <utkarsh867@gmail.com>
2022-10-08 16:04:59 +0800

    Add logging to the console for actix_web logs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
 use std::env;
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
+use env_logger;
 
 pub mod routes;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+  env::set_var("RUST_LOG", "debug");
+  env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
   let port = match env::var("port") {
     Ok(port) => port.parse::<u16>().unwrap(),
     Err(_) => 4000
   };
 
   HttpServer::new(|| {
-    App::new().configure(routes::initialise)
+    App::new()
+    .configure(routes::initialise)
+    .wrap(Logger::new("%t %a %r %s %{User-Agent}i"))
   }).bind((String::from("0.0.0.0"), port))?
     .run()
     .await
